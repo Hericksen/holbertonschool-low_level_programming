afb323ec30c0e31f1fee37ce3411e6670026f608
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 15:18:08 +0000

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-  int digit;
-
-  for (digit =0; digit <=9; digit ++)
-    {
-      putchar (digit);
-      putchar (',');
-      putchar (' ');
+int digit;
+for (digit = 0; digit <= 9; digit ++)
+{
+putchar(digit + '0');
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
      
-    }
-  putchar ('\n');
-
-  return (0);
+}
+putchar ('\n');
+return (0);
 }
  
