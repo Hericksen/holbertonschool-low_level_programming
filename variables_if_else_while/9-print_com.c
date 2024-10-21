eaaf5d96b295eeca3bd198166329147254dc38cd
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 15:08:55 +0000

    Task 9

--- variables_if_else_while/9-print_com.c
+++ variables_if_else_while/9-print_com.c
@@ -3,9 +3,19 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-  
+  int digit;
+
+  for (digit =0; digit <=9; digit ++)
+    {
+      putchar (digit);
+      putchar (',');
+      putchar (' ');
+     
+    }
+  putchar ('\n ');
 }
+ 
