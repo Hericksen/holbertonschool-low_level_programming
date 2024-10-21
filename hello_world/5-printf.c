ecae4f877092b15c6e78dc8fe889ee552fe919ef
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 09:52:25 +0000

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : Display a message
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper gramma, but the outcome is a piece of art");
+return (0);
+}
