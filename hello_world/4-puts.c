29417db0ee8b2a6acf26c67299c834bb016b811b
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 09:00:29 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/*Main fuction*/
+int main(void)
+{  puts("Programming is like building a multilingual puzzle");
+return (0);
+}
