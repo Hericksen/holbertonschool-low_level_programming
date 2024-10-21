d0e3df237ab7ac2b7c76d7080f93eef84111028b
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 10:13:35 +0000

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-  printf("Size of a char : 1 byte(s)\n");
-  printf("Size of an int : 4 byte(s)\n");
-  printf("Size of a long int : 4 byte(s)\n");
-  printf("Size of a long long int : %ll byte(s)\n");
-  printf("Size of a float : 4 byte(s)");
+  printf("Size of a char : %c byte(s)\n", sizeof(char));
+  printf("Size of an int : %d byte(s)\n", sizeof(int));
+  printf("Size of a long int : %ld byte(s)\n", sizeof(long int));
+  printf("Size of a long long int : %ll byte(s)\n", sizeof(long long int));
+  printf("Size of a float : %f byte(s)", sizeof(float));
   return (0);
 }
