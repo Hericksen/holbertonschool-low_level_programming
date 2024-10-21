3c441361b12b7447e98b4fb880dbd13b8c098807
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-21 09:07:31 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-/*Main fuction*/
+/*
+ *
+ * main - Entry point of every C program
+ *
+ * Description : Calls puts and revieves returned value by puts
+ * after his execution
+ *
+ *
+ * Return : Always return 0 to indicate success
+ */
+
 int main(void)
-{  puts("Programming is like building a multilingual puzzle");
-return (0);
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
 }
