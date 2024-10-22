bb2c86f23c047a64158f33bfa1a2ca6ca5b29318
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 11:39:45 +0000

    Task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,10 @@ int print_last_digit(int d)
 {
 	int last_digit;
 	last_digit = d % 10;
+	if (last_digit < 0)
+	{
+		last_digit -= last_digit;
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
