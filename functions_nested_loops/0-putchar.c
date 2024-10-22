5ea19f8bc2dc860f995e045f427165a64906a438
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 08:32:26 +0000

    Task 0 & README" git push

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	putchar('_');
+        putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
