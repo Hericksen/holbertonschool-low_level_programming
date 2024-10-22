bd43eac4827f6f0a61c377efa86cfbb63909e9d0
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 12:23:53 +0000

    Task 8

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 			_putchar(' ');
 
 			product = number * multiplication;
-			
+
 			if (product <= 9)
 		        {
 				_putchar(' ');
@@ -27,6 +27,7 @@ void times_table(void)
 			{
 				_putchar((product/10) + '0');
 			}
+			_putchar((prod % 10) + '0');
 		}
 	}
 }
