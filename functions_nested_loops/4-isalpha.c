61051b6e8d5b9c7021d1e6b9f7d82ffde312ad52
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 10:37:19 +0000

    Task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,20 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
- *
+ * _isalpha - Fucntion testing if character is alpha
+ * @c: Character tested
+ * Return: 1 if true 0 if falser
 */
 
 int _isalpha(int c)
 {
-	
+	if(isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
