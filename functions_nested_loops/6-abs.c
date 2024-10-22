addb98b303f3d11cc3b3f3e83ba77016d5feac03
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 10:58:40 +0000

    Task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
-#include "main."
+#include "main.h"
 #include <stdlib.h>
 
 /**
  * _abs - computes absolutes value of an integer
- * return: return its value 
+ * return: return its value
 */
 
 int _abs(int);
