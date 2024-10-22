086dd2c6491b28dd669fcaa42b96039acdeb93c7
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 12:32:49 +0000

    Task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- *
+ * add - add two integers and returns the result
+ * @a: first integers
+ * @b: second integers
+ * Return: return the result of a + b
 */
 
 int add(int a, int b)
