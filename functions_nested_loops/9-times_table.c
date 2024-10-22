e692f7fda4c4b83c39d28d7dff5369c2674ba1ce
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 12:28:54 +0000

    Task 8

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *
+ * times_table - display the 9 table
 */
 
 void times_table(void)
@@ -20,12 +20,12 @@ void times_table(void)
 			product = number * multiplication;
 
 			if (product <= 9)
-		        {
+			{
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar((product/10) + '0');
+				_putchar((product / 10) + '0');
 			}
 			_putchar((product % 10) + '0');
 		}
