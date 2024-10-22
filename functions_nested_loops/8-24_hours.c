5605ef16d6311e5260f0c3061b2255b0aba721d8
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-22 11:58:18 +0000

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar((hours / 10) + '0');
-			_putchar(hours + '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((minutes /10) + '0');
-			_putchar(minutes + '0');
-			_putchar('\n');
+			_putchar((minutes %10) + '0');
+			_Putchar('\n');
 		}
 	}
 }
