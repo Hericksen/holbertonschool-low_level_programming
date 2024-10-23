1cf1c14042d2a95824137e38ce8ec4ba07fa00b6
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-23 13:03:04 +0000

    Task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: always 0
+*/
+
+int main (void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
