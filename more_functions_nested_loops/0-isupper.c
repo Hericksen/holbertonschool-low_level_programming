feefc869c83572299bf2d1b92a3367ded00398f2
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-28 09:01:29 +0000

    Task 0 & README"

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: character checked
+ * Return: 1 if upper, 0 if not
+*/
+
+int _isupper (int c)
+{
+	if (isupper(c))
+		return (1);
+	return (0);
+}
