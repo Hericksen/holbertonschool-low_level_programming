898825d46d5f837d2889458519a18cbc161fd238
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-28 14:16:14 +0000

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0 && i != 100)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if (i % 15 == 0 && i == 100)
-			printf("FizzBuzz");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i != 100)
 			printf("Buzz ");
+		else if (i % 5 == 0 && i == 100)
+			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
