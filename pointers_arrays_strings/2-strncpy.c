158f7c13288d706bb845b637530a0edb00bc6a01
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-30 14:11:38 +0000

    Task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "2-strlen.c"
+/**
+ *
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
