7412e8a8fe6449b63f06a653a4fcaeaf9d50a1b3
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-30 09:01:39 +0000

    Task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,17 @@
  * _strcpy - copy a string into another
  * @dest: destination string
  * @src: source string
+ * Return: Pointer to dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	char *dest_start = dest;
 
-	while(*src != '\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
-		dest ++;
+		dest++;
 		src++;
 	}
 
