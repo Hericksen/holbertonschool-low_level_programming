a7bb029cd9ca2a0b3f6fad0c796bb92bed81e1d5
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-31 11:02:14 +0000

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,10 @@
 #include "2-strlen.c"
 /**
- *
+ * _strncpy - copy the string
+ * @dest: string destination
+ * @src: string source
+ * @n: size of string in byte
+ * Return: return the string
 */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -8,9 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
-
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
