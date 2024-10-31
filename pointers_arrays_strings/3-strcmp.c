9f0172e7082b744bcb5d30515de62662c53df8e5
Hericksen <gwendal.martin35@hotmail.fr>
2024-10-31 12:06:02 +0000

    Task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
-*
+* _strcmp - compare two strings
+* @s1: 1st string
+* @s2: 2nd string
+* Return: 0 if s1 = s2, negative valur if s1 < s2, positive value otherwise
 */
 
 int _strcmp(char *s1, char *s2)
@@ -10,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s1[1] != s2[i])
+		if (s1[i] != s2[i])
 		{
 			compare = s1[i] - s2[i];
 			break;
