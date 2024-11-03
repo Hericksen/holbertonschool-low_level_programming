4aeffec65dc46679370bed04bd9d1854bb8febb4
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-03 15:13:54 +0000

    Task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,12 +2,22 @@
 
 
 /**
- *
+ * _atoi - convert a string into an int
+ * @str: string converted
+ * Return: the result of checked number
 */
 
 int _atoi(char *str)
 {
 	int result = 0;
+	int sign = 1;
+
+	while (*str && (*str < '0' || *str > '9'))
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
 
 	while (*str >= '0' && *str <= '9')
 	{
