f4f996c1ac52a777de41abf38064d455fdc19b85
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-05 08:31:31 +0000

    Tasks

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,11 +7,11 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 		sum1 += a[i * size + i];
 	for (i = 0; i < size; i++)
-		sum2 += a[i * size + (size - i)];
+		sum2 += a[i * size + (size - i - 1)];
 	printf ("%d, %d\n", sum1, sum2);
 }
