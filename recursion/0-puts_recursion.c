f67739d7070d860f641cba56312880d4e9e6f12b
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 10:06:22 +0100

    Task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - prints a string by recursion
- * @s: string print
+ * @s: pointer to string
  */
 
 void _puts_recursion(char *s)
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
     if (*s == '\0')
     {
         _putchar('\n');
-        return;
+        return (0);
     }
     _putchar(*s);
     _puts_recursion(s + 1);
