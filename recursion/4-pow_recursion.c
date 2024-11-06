c59348424d68dffd579406c94d20c1980c724745
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 09:39:34 +0000

    Task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,19 +2,18 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * 
  * @x: number
  * @y: power
- * Return: if y positive return 1, if y negative return -1, 
- * else return power 
+ * Return: if y positive return 1, if y negative return -1,
+ * else return power
  */
 
 int _pow_recursion(int x, int y)
 {
-    if (y == 0)
-        return (1);
-    else if (y < 0)
-        return (-1);
-    else
-        return (x * _pow_recursion(x, y - 1));
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
