954ac86eac690f1a50c52d115a4f1334a561da6c
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 11:17:38 +0100

    Tasks

--- recursion/0-main.c
+++ recursion/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,9 +7,6 @@
  */
 int main(void)
 {
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
+    _puts_recursion("Puts with recursion");
     return (0);
 }
