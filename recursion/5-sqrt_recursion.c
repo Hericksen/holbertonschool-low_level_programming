bbbc76440bbc09f56fe21932d6cbded246eb10a1
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 09:42:13 +0000

    Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-
 /**
  *  root_square - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  * Return: On success 1, on error, -1 is returned
  */
+
 int root_square(int a, int b)
 {
 	if (b * b == a)
@@ -15,11 +15,13 @@ int root_square(int a, int b)
 		return (-1);
 	return (root_square(a, b + 1));
 }
+
 /**
  * _sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
  * Return: On success 1, on error, -1 is returned.
  */
+
 int _sqrt_recursion(int n)
 {
 	return (root_square(n, 1));
