bb4bfe4fd873db83f7adbe75957931177342d144
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 09:42:01 +0100

    Task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string 
- * by recursion 
- * 
- * @s: string tested 
+ * _strlen_recursion - returns the length of a string
+ * by recursion
+ * @s: string tested
  * Return: 0 if end of the string, 1 + recursivity
  */
+
 int _strlen_recursion(char *s)
 {
     if (*s == '\0')
         return (0);
-    else 
+    else
         return (1) + _strlen_recursion(s + 1);
 }
