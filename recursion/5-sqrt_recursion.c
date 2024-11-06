144ceca76fe6f11f0a9b439ee264c3e10426a514
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 09:29:25 +0100

    Task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ *  - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: On success 1, on error, -1 is returned
+ */
+int root_square(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (root_square(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ * Return: On success 1, on error, -1 is returned.
+ */
+int _sqrt_recursion(int n)
+{
+	return (root_square(n, 1));
+}
