d00556a82415446b4364e40879cae0ba21e73bf1
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-06 13:04:04 +0000

    Task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* main - entry point
+/**
+ * main - entry point
  * @argc: all arguments
  * @argv: array of arguments
  * Return: Error if more than 2 args, 0 if 2 args.
@@ -10,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
