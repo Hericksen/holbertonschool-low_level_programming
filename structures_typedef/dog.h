90ccddf75902f34b88e7117ef6b021dadb2fb1e2
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-13 12:32:20 +0000

    Bug

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _struct
-#define _struct
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdlib.h>
 
@@ -20,5 +20,5 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-
+void free_dog(dog_t *d);
 #endif
