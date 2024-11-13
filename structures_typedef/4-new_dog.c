04564fb7bf9f3179171c36febeb1dedc0fa57e75
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-13 12:21:18 +0000

    Dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *copy_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (copy_dog == NULL)
+		return (NULL);
+
+	copy_dog->name = (char *)malloc(strlen(name) + 1);
+
+	if (copy_dog->name == NULL)
+	{
+		free(copy_dog);
+		return (NULL);
+	}
+
+	copy_dog->owner = (char *)malloc(strlen(owner) + 1);
+
+	if (copy_dog->owner == NULL)
+	{
+		free(copy_dog->name);
+		free(copy_dog);
+		return (NULL);
+	}
+
+	strcpy(copy_dog->name, name);
+	strcpy(copy_dog->owner, owner);
+
+	copy_dog->age = age;
+
+	return (copy_dog);
+}
