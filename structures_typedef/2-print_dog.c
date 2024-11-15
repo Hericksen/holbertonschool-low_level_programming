2dbf28a238b8d37a333e034c9f327dc53ef23a2f
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-15 08:35:30 +0000

    Test

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,12 +10,26 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	/*if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
 		printf("Owner : %s\n", (d->owner != NULL) ? d->owner : "(nil)");
-	}
-	else
+	}*/
+
+	if (d == NULL)
 		return;
+	printf("Name: ");
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->name);
+	printf("Age: ");
+
+	printf("%f\n", d->age);
+	printf("Owner: ");
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->owner);
 }
