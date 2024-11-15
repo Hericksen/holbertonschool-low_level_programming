8e9aa5fc2ccbfc87d0f79e857b0e7de3c159cecd
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-15 08:54:26 +0000

    Test

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,18 +9,19 @@
 
 int *array_range(int min, int max)
 {
-	int i;
-	int range_size = max - min + 1;
-	int *array = (int *)malloc(range_size * sizeof(int));
+	int *ptr;
+	int i = 0;
 
 	if (min > max)
 		return (NULL);
 
-	if (array == NULL)
+	ptr = malloc((sizeof(int) * (max - min) + sizeof(int)));
+
+	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < range_size; i++)
-		array[i] = min + i;
+	while (min <= max)
+		ptr[i++] = min++;
 
-	return (array);
+	return (ptr);
 }
