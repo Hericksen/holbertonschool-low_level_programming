0d5ea4f5be14c6ecc455e9111e3958be05aa0876
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-19 12:04:59 +0000

    Task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg.h>
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
 #endif
