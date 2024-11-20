efed1b85dc32a6e1d1b7ae0346551eae108f4d83
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-20 08:36:43 +0000

    Betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,10 @@
 #include "3-calc.h"
 
 /**
- *
+ * main - entry point, program to find an operation
+ * @argc: number of argument
+ * @argv: list of argument
+ * Return: 0 Success
 */
 
 int main(int argc, char *argv[])
@@ -19,7 +22,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]); /* Convert the 1st arg to an int */
 	num2 = atoi(argv[3]); /* Convert the 3rd arg to an int */
-	op = get_op_func(argv[2]); /* Get the hnc corresponding to the op */
+	op = get_op_func(argv[2]); /* Get the fnc corresponding to the op */
 
 	if (!op) /* Check if valid op */
 	{
@@ -27,8 +30,8 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 
-	/* Check if division/modulo by 0 */
-	if ((argv[2][0] == '/' || argv [2][0] == '%') && num2 == 0)
+	/* Check if the operation will divide/modulate by 0 */
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		return (100);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,37 +2,72 @@
 #include <stdio.h>
 #include "3-calc.h"
 
+/**
+ * op_add - Addition of 2 int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Addition of 2 int
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - Substraction of 2 int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Substraction of 2 int
+ */
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+  * op_mul - Multiplication of 2 int
+  * @a: 1st int
+  * @b: 2nd int
+  * Return: Multiplication of 2 int
+  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - Division of 2 int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Division of 2 int
+ */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Can't divide by 0");
-		exit(1);
+		printf("Return\n");
+		exit(100);
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod - Modulo of 2 int
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Modulo of 2 int
+ */
 
 int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Can't modulate by 0");
-		exit(1);
+		printf("Error\n");
+		exit(100);
 	}
 	return (a % b);
 }
