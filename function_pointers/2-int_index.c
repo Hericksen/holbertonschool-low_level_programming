ad955e1a0ef96fd41dc0d3bcbe1d7662fae1d781
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-20 08:15:13 +0000

    Betty

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,8 +1,12 @@
 #include <stddef.h>
 
 /**
-
-*/
+ * int_index - Search an integer in an array
+ * @array: Array source
+ * @size: Size of array
+ * @cmp: Function use to compare in array
+ * Return: -1 if false, else return the int.
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -12,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	for (i = 0; i < size; i++)
-		if(cmp(array[i]) !=0)
+		if (cmp(array[i]) != 0)
 			return (i);
 	return (-1);
 }
