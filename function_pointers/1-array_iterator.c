a3efb2cebe0162971b13cb6a4a7088d101ea7e12
Hericksen <gwendal.martin35@hotmail.fr>
2024-11-20 08:12:23 +0000

    Betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 
 /**
-*/
+ * array_iterator - executes a fnc given as a parameter on each element of arr
+ * @array: array of integer
+ * @size: size of array
+ * @action: pointer to function
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if(array == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
