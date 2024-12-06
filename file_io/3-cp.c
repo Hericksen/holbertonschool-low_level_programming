3aa632cf104d180551c11c000f11037e42c50537
Hericksen <gwendal.martin35@hotmail.fr>
2024-12-06 18:51:28 +0100

    Add of a variable to ensure all bytes written

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, num;
+	int file_from, file_to, num, bytes_written;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -21,30 +21,31 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-
+	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 		close(file_from), exit(99);
 
 	while ((num = read(file_from, buffer, 1024)) > 0)
-		if (write(file_to, buffer, num) != num)
+	{
+		bytes_written = write(file_to, buffer, num);
+		if (bytes_written != num) /* Ensure all bytes are written correctly */
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 			close(file_from), close(file_to), exit(99);
-
+	}
 	if (num == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		close(file_from), close(file_to), exit(98);
 
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from),
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to),
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
