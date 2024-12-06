82c6029f0a939299692761ef9f9f53ee6fd8888f
Hericksen <gwendal.martin35@hotmail.fr>
2024-12-06 18:20:31 +0100

    Fix 40 lines for Betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,6 @@
  * main - Copies the content of a file to another file.
  * @argc: Number of arguments.
  * @argv: Array of argument strings.
- *
  * Return: 0 on success, exits with error code otherwise.
  */
 int main(int argc, char *argv[])
@@ -16,36 +15,21 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 		close(file_from), exit(99);
-	}
 	while ((num = read(file_from, buffer, 1024)) > 0)
-	{
 		if (write(file_to, buffer, num) != num)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(file_from);
-			close(file_to);
-			exit(99);
-		}
-	}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
+			close(file_from), close(file_to), exit(99);
 	if (num == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(file_from);
-		close(file_to);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+		close(file_from), close(file_to), exit(98);
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
