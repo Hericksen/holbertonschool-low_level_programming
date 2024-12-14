e69a1cc26bbf858beacd3bf3f33ab5f9f317f248
Hericksen <gwendal.martin35@hotmail.fr>
2024-12-14 12:57:59 +0100

    Task 102

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -3,10 +3,10 @@
 int main() {
     int count = 50;
     int first = 1, second = 2, next;
-
+    int i = 0;
     printf("%d, %d", first, second);
 
-    for (int i = 3; i <= count; i++) {
+    for (i = 3; i <= count; i++) {
         next = first + second;
         printf(", %d", next);
         first = second;
